1dc90dc6768134fad698fceebd30ac8fd31fd131
acm818 <acm818@gmail.com>
2020-11-10 13:53:34 -0500

    Interaction with table established
    Tiger Park can now connect to a database and pull data out of a table

--- index.php
+++ index.php
@@ -4,15 +4,28 @@
   $servername = "localhost";
   $username = "root";
   $password = "";
+  $db = "tiger_park";
 
   // Create connection
-  $conn = mysqli_connect($servername, $username, $password);
+  $conn = mysqli_connect($servername, $username, $password, $db);
 
   // Check connection
   if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
   }
   echo "Connected successfully";
+
+  $sql = "SELECT id, fname, lname FROM test";
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+      echo "id: " . $row["id"]. " - Name: " . $row["fname"]. " " . $row["lname"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
 
 
 ?>
